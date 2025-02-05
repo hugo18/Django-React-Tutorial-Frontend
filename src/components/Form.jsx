372584f4fb3142c66e7970abfd358c09633f57ab
Hugo Alves Silva <hugoalves@egresso.ufg.br>
2025-02-05 20:40:45 -0300

    Register, Login e Logout

---
+++ src/components/Form.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import api from '../api'
+import { useNavigate } from 'react-router-dom'
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'
+import '../styles/Forms.css'
+
+function Form({route, method}){
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState('')
+    const navigate = useNavigate()
+    
+    const name = method === 'login' ? 'Login' : 'Register'
+    const handleSubmit = async (e) => {
+        setLoading(true)
+        e.preventDefault()
+        try{
+            const res = await api.post(route, {username, password})
+            if(method === 'login'){
+                localStorage.setItem(ACCESS_TOKEN, res.data.access)
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
+                navigate('/')
+            }else{
+                navigate('/login')
+            }
+
+        }catch(error){
+            alert(error)
+        }finally {
+            setLoading(false)
+        }
+    
+    }  
+    return <form onSubmit={handleSubmit} className = 'form-container'>
+        <h1>{name}</h1>
+        <input className='form-input' type='text' value={username} onChange={(e)=>setUsername(e.target.value)} placeholder='Nome de usuário'></input>
+        <input className='form-input' type='password' value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Senha'></input>
+        <button className='formbutton' type='subimit'>
+            {name}
+        </button>
+
+
+    </form>
+
+}
+export default Form
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+function NotFound(){
+    return <div>
+        <h1>404 Página não encontrada</h1>
+        <p>A pagina que você está procurando não existe!</p>
+        </div>
+}
+export default NotFound
